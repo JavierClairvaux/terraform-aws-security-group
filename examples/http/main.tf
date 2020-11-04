c4f13837bf4ce27a97a2b4dbf4d2ea1fa22c3e7e
JavierClairvaux <javcabr@gmail.com>
2020-11-04 10:49:32 -0800

    new examples

--- examples/http/main.tf
+++ examples/http/main.tf
@@ -1,5 +1,5 @@
 provider "aws" {
-  region = "eu-west-1"
+  region = "us-east-2"
 }
 
 #############################################################
@@ -21,88 +21,39 @@ data "aws_security_group" "default" {
 #######
 # HTTP
 #######
+
 module "http_sg" {
-  source = "../../modules/http-80"
+  source = "../../"
 
   name        = "http-sg"
   description = "Security group with HTTP ports open for everybody (IPv4 CIDR), egress ports are all world open"
   vpc_id      = data.aws_vpc.default.id
 
-  ingress_cidr_blocks = ["0.0.0.0/0"]
+  ingress_rules = ["https-443-tcp", "ssh-tcp"]
+
+  ingress_cidr_blocks = ["10.1.0.0/16", "10.2.0.0/16"]
 }
 
-#####################
-# HTTP with MySQL #1
-#####################
-module "http_mysql_1_sg" {
-  source = "../../modules/http-80"
+module "user_service" {
+  source = "../../"
 
-  name            = "http-mysql-1"
-  use_name_prefix = false
-
-  description = "Security group with HTTP and MySQL ports open for everybody (IPv4 CIDR)"
+  name        = "user-service"
+  description = "Security group for user-service with custom ports open within VPC, and PostgreSQL publicly open"
   vpc_id      = data.aws_vpc.default.id
 
-  ingress_cidr_blocks = ["0.0.0.0/0"]
-
-  # Add MySQL rules
-  ingress_rules = ["mysql-tcp"]
+  ingress_cidr_blocks = ["10.10.0.0/16"]
+  ingress_rules       = ["https-443-tcp"]
+  ingress_with_cidr_blocks = [
+    {
+      from_port   = 8080
+      to_port     = 8090
+      protocol    = "tcp"
+      description = "User-service ports"
+      cidr_blocks = "10.10.0.0/16"
+    },
+    {
+      rule        = "ssh-tcp"
+      cidr_blocks = "0.0.0.0/0"
+    },
+  ]
 }
-
-#####################
-# HTTP with MySQL #2
-#####################
-module "http_mysql_2_sg" {
-  source = "../../modules/http-80"
-
-  name        = "http-mysql-2"
-  description = "Security group with HTTP and MySQL ports open within current VPC"
-  vpc_id      = data.aws_vpc.default.id
-
-  # Add mysql rules
-  ingress_rules = ["mysql-tcp"]
-
-  # Allow ingress rules to be accessed only within current VPC
-  ingress_cidr_blocks      = [data.aws_vpc.default.cidr_block]
-  ingress_ipv6_cidr_blocks = [] # Not all VPCs have IPv6 enabled, but if you have it enabled, then this will work - ["${data.aws_vpc.default.ipv6_cidr_block}"]
-}
-
-###########################
-# HTTP with egress minimal
-###########################
-module "http_with_egress_minimal_sg" {
-  source = "../../modules/http-80"
-
-  name        = "http-with-egress-minimal"
-  description = "Security group with HTTP ports open within current VPC, and allow egress access to HTTP ports to the whole world"
-  vpc_id      = data.aws_vpc.default.id
-
-  # Allow ingress rules to be accessed only within current VPC
-  ingress_cidr_blocks = [data.aws_vpc.default.cidr_block]
-
-  # Allow all rules for all protocols
-  egress_rules = ["http-80-tcp"]
-}
-
-###########################
-# HTTP with egress limited
-###########################
-module "http_with_egress_sg" {
-  source = "../../modules/http-80"
-
-  name        = "http-with-egress"
-  description = "Security group with HTTP ports open within current VPC, and allow egress access just to small subnet"
-  vpc_id      = data.aws_vpc.default.id
-
-  # Add mysql rules
-  ingress_rules = ["mysql-tcp"]
-
-  # Allow ingress rules to be accessed only within current VPC
-  ingress_cidr_blocks      = [data.aws_vpc.default.cidr_block]
-  ingress_ipv6_cidr_blocks = [] # Not all VPCs have IPv6 enabled, but if you have it enabled, then this will work - ["${data.aws_vpc.default.ipv6_cidr_block}"]
-
-  # Allow egress rules to access anything (empty list means everything)
-  egress_cidr_blocks      = ["10.10.10.0/28"]
-  egress_ipv6_cidr_blocks = [] # Not all VPCs have IPv6 enabled, but if you have it enabled, then this will work - ["${data.aws_vpc.default.ipv6_cidr_block}"]
-}
-
